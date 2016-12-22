4a0a5d56c411daa9ca7fb9f582937961468553e9
Axeldoomfist <morgan12we@hotmail.co.uk>
2016-12-22 14:57:25 +0000

    Made some changes to the javascript

--- main.js
+++ main.js
@@ -1,15 +1,46 @@
 var handler = (form) => {
-    fetch('/usr', {
-            method: 'POST',
+    getFormData(form)
+    .then(sendFormData(form))
+    .then((response) => {
+        document.getElementById('uid').innerHTML = response.uid
+        alert('Account creation success')
+        return false
+    })
+    .catch((error) => {
+        return false
+    })
+}
+
+function sendFormData (form) {
+    return new Promise((resolve, reject) => {
+        fetch('http://127.0.0.1:5000/usr', {
+            method: 'PUT',
+            mode: 'cors',
             headers: {
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify(form)
         })
-        .then((response) => {
-            if (!response.success) {
-                throw 'It broke'
-            }
-        })
-    return false;
+            .then((response) => {
+                return response.json()
+            })
+            .then((response) => {
+                if (response.success === false) {
+                    throw new Error({message: 'Account was not successfully created'})
+                } else {
+                    resolve(response)
+                }
+            })
+    })
+}
+
+function getFormData (submitData) {
+    return new Promise((resolve, reject) => {
+        if (!submitData) reject('No form data received.')
+        data = {}
+        for (element in submitData) {
+            data[`${element.id}`] = element.value
+        }
+        resolve(data)
+    })
 }
