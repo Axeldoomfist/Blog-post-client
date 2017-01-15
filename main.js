6b9ed77d8e8ba5bc373d3d0123af0249883933ff
Axeldoomfist <morgan12we@hotmail.co.uk>
2017-01-15 16:13:17 +0000

    Changes to the HTML and reconfigured javascript

--- main.js
+++ main.js
@@ -1,46 +1,23 @@
-var handler = (form) => {
-    getFormData(form)
-    .then(sendFormData(form))
-    .then((response) => {
-        document.getElementById('uid').innerHTML = response.uid
-        alert('Account creation success')
-        return false
-    })
-    .catch((error) => {
-        return false
-    })
-}
+function main(form) {
+	const xhr = new XMLHttpRequest();
+	// Define Variables
+	const user = {};
+	user.username = form.usr.value;
+	user.password = form.pswd.value;
+	user.email = form.email.value;
+	user.name = form.name.value;
+	const send = JSON.stringify(user);
+	xhr.open('PUT', "//127.0.0.1:5000/usr", true);
+	xhr.send(send);
+	xhr.addEventListener("readystatechange", processRequest, true);
+	xhr.onreadystatechange = processRequest;
 
-function sendFormData (form) {
-    return new Promise((resolve, reject) => {
-        fetch('http://127.0.0.1:5000/usr', {
-            method: 'PUT',
-            mode: 'cors',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(form)
-        })
-            .then((response) => {
-                return response.json()
-            })
-            .then((response) => {
-                if (response.success === false) {
-                    throw new Error({message: 'Account was not successfully created'})
-                } else {
-                    resolve(response)
-                }
-            })
-    })
-}
+	function processRequest(e) {
+		if (xhr.readyState == 4 && xhr.status == 200) {
+			const response = JSON.parse(xhr.responseText);
+			document.getElementById('uid').innerHTML = response.user_id;
+		}
+	}
 
-function getFormData (submitData) {
-    return new Promise((resolve, reject) => {
-        if (!submitData) reject('No form data received.')
-        data = {}
-        for (element in submitData) {
-            data[`${element.id}`] = element.value
-        }
-        resolve(data)
-    })
+	alert(send);
 }
